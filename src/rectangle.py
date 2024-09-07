471a75aa8f83695347079b4ae50bfc725e852f0d
user <batrakova-olesya@inbox.ru>
2024-09-07 19:46:16 +0300

    the consistency of the code

--- src/rectangle.py
+++ src/rectangle.py
@@ -1,2 +1,43 @@
+from abc import ABC, abstractmethod
 
-# Каждая фигура должна иметь методы для вычисления площади(get_area()) и периметра(get_perimeter())
+
+class Figure(ABC):
+    @abstractmethod
+    def get_area(self):
+        pass
+
+    @abstractmethod
+    def get_perimeter(self):
+        pass
+
+    def add_area(self, other_figure):
+        if not isinstance(other_figure, Figure):
+            raise ValueError("Should be a Figure")
+        return self.get_area + other_figure.get_area
+
+
+class Rectangle(Figure):
+    def __init__(self, side_a, side_b):
+        if side_a <= 0 or side_b <= 0:
+            raise ValueError("Rectangle sides can't be less than 0")
+        self.side_a = side_a
+        self.side_b = side_b
+
+    @property
+    def get_area(self):
+        return self.side_a * self.side_b
+
+    @property
+    def get_perimeter(self):
+        return (self.side_a + self.side_b) * 2
+
+class Square(Rectangle):
+    def __init__(self, side_a):
+        if side_a <= 0:
+            raise ValueError("Square sides can't be less than 0")
+        super().__init__(side_a, side_a)
+
+r = Rectangle(3, 5)
+s = Square(5)
+print(r.add_area(s))
+print(s.add_area(r))
--- src/square.py
+++ src/square.py
@@ -1,2 +1,24 @@
+from abc import ABC, abstractmethod
 
-# Каждая фигура должна иметь методы для вычисления площади(get_area()) и периметра(get_perimeter())
+
+class Figure(ABC):
+    @abstractmethod
+    def get_area(self):
+        pass
+
+    @abstractmethod
+    def get_perimeter(self):
+        pass
+
+    def add_area(self, other_figure):
+        if not isinstance(other_figure, Figure):
+            raise ValueError("Should be a Figure")
+        return self.get_area + other_figure.get_area
+class Square:
+    def __init__(self, side_a):
+        if side_a <= 0:
+            raise ValueError("Square sides can't be less than 0")
+        super().__init__(side_a, side_a)
+
+s = Square(5)
+print(s.add_area)
