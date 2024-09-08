1b7cff113fbd5d2853fdac5b5844c6c557aa70f9
user <batrakova-olesya@inbox.ru>
2024-09-08 18:03:34 +0300

    commit new def

--- src/triangle.py
+++ src/triangle.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-
+import math
 
 class Figure(ABC):
     @abstractmethod
@@ -15,14 +15,31 @@ class Figure(ABC):
             raise ValueError("Should be a Figure")
         return self.get_area + other_figure.get_area
 class Triangle(Figure):
-    def __init__(self, side_a, side_b, side_c, side_h):
-        if side_a <= 0 or side_b <= 0 or side_c <= 0 or side_h <= 0:
+    def __init__(self, side_a, side_b, side_c, side_h, side_p):
+        if side_a <= 0 or side_b <= 0 or side_c <= 0 or side_h <= 0 or side_p <= 0:
             raise ValueError("Triangle sides can't be less than 0")
         self.side_a = side_a
         self.side_b = side_b
         self.side_c = side_c
         self.side_h = side_h
+        self.side_p = side_p
 
+    @property
+    def half_meter(self, side_p):
+        return (self.side_a + self.side_b + self.side_c) / 2
+
+    @property
+    def height(self, side_h):
+        return 2 * (math.cbrt(self.side_p * (self.side_p - self.side_a) * (self.side_p - self.side_b)
+                              * (self.side_p - self.side_c))) / self.side_a
+
+    @property
+    def get_area(self):
+        return 0.5 * self.side_a * self.side_h
+
+    @property
+    def get_perimeter(self):
+        return self.side_a + self.side_b + self.side_c
 
 class Square(Triangle):
     def __init__(self, side_a):
