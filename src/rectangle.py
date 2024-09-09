5c33c7f5a63e7525ece1af8e3c0898bb85162810
user <batrakova-olesya@inbox.ru>
2024-09-09 10:01:44 +0300

    format

--- src/rectangle.py
+++ src/rectangle.py
@@ -13,21 +13,19 @@ class Figure(ABC):
     def add_area(self, other_figure):
         if not isinstance(other_figure, Figure):
             raise ValueError("Should be a Figure")
-        return self.get_area + other_figure.get_area
+        return self.get_area() + other_figure.get_area()
 
 
 class Rectangle(Figure):
     def __init__(self, side_a, side_b):
         if side_a <= 0 or side_b <= 0:
-            raise ValueError("Rectangle sides can't be less than 0")
+            raise ValueError("Rectangle sides can't be less than or equal to 0")
         self.side_a = side_a
         self.side_b = side_b
 
-    @property
     def get_area(self):
         return self.side_a * self.side_b
 
-    @property
     def get_perimeter(self):
         return (self.side_a + self.side_b) * 2
 
@@ -35,7 +33,7 @@ class Rectangle(Figure):
 class Square(Rectangle):
     def __init__(self, side_a):
         if side_a <= 0:
-            raise ValueError("Square sides can't be less than 0")
+            raise ValueError("Square sides can't be less than or equal to 0")
         super().__init__(side_a, side_a)
 
 
