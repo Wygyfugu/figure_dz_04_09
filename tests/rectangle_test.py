eb0b506a6b758bd80be528b1be257bd75c75f4e0
user <batrakova-olesya@inbox.ru>
2024-09-10 16:31:47 +0300

    delete #

--- tests/rectangle_test.py
+++ tests/rectangle_test.py
@@ -1,12 +1,12 @@
-# from src.Rectangle import Rectangle
-# import pytest
+from src.Rectangle import Rectangle
+import pytest
 
-# @pytest.mark.smoke
-# def test_rectangle_integer():
-# r = Rectangle(side_a:3, side_b:5)
-# assert r.get_area == 15, 'Area should be 15'
+@pytest.mark.smoke
+def test_rectangle_integer():
+r = Rectangle(side_a:3, side_b:5)
+assert r.get_area == 15, 'Area should be 15'
 
-# @pytest.mark.slow
-# def test_rectangle_float():
-# r = Rectangle(side_a:3.5, side_b:5.5)
-# assert r.get_area == 19.25, 'Area should be 19.25'
+@pytest.mark.slow
+def test_rectangle_float():
+r = Rectangle(side_a:3.5, side_b:5.5)
+assert r.get_area == 19.25, 'Area should be 19.25'
