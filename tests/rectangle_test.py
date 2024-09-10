329a2b534c6c8ea9de054770938fa3a5ec4315d7
user <batrakova-olesya@inbox.ru>
2024-09-10 20:53:39 +0300

    marks

--- tests/rectangle_test.py
+++ tests/rectangle_test.py
@@ -2,13 +2,14 @@ from src.rectangle import Rectangle
 import pytest
 
 
-@pytest.mark.parametrise(
+@pytest.mark.parametrize(
     "side_a, side_b, area",
     [
-        (3, 5, 15),
+        pytest.param(3, 5, 15, marks=pytest.mark.skip),
         (3.5, 5.5, 19.25)
-    ]
+    ],
+    ids=["integer", "float"]
 )
-def test_rectangle_positive(side_a, side_b, area):
+def test_rectangle_area_positive(side_a, side_b, area):
     r = Rectangle(side_a, side_b)
     assert r.get_area == area, f"Area should be {area}"
