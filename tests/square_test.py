e6eed9f25ca9940e9d878df544d53fbe696efc2f
user <batrakova-olesya@inbox.ru>
2024-09-16 16:13:30 +0300

    parametrize square_test.py

--- tests/square_test.py
+++ tests/square_test.py
@@ -2,25 +2,41 @@ import pytest
 from src.square import Square
 
 
-def test_square_initialization_valid():
-    square = Square(5)
-    assert square.side_a == 5
+@pytest.mark.parametrize("side_a", [5])
+def test_square_initialization_valid(side_a):
+    square = Square(side_a)
+    assert square.side_a == side_a
 
 
-def test_square_initialization_invalid():
-    with pytest.raises(ValueError, match="Square sides can't be less than or equal to 0"):
-        Square(0)
-    with pytest.raises(ValueError, match="Square sides can't be less than or equal to 0"):
-        Square(-1)
+@pytest.mark.parametrize("side_a", [0, -1])
+def test_square_initialization_invalid(side_a):
+    with pytest.raises(
+        ValueError, match="Square sides can't be less than or equal to 0"
+    ):
+        Square(side_a)
 
 
-def test_square_get_area():
-    square = Square(5)
-    expected_area = 5**2
+@pytest.mark.parametrize(
+    "side_a, expected_area",
+    [
+        (5, 25),
+        (3, 9),
+        (7, 49),
+    ],
+)
+def test_square_get_area(side_a, expected_area):
+    square = Square(side_a)
     assert square.get_area() == expected_area
 
 
-def test_square_get_perimeter():
-    square = Square(5)
-    expected_perimeter = 4 * 5
+@pytest.mark.parametrize(
+    "side_a, expected_perimeter",
+    [
+        (5, 20),
+        (3, 12),
+        (7, 28),
+    ],
+)
+def test_square_get_perimeter(side_a, expected_perimeter):
+    square = Square(side_a)
     assert square.get_perimeter() == expected_perimeter
