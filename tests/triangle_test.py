7dda9b2c0cb8c465dc6e23beb6184010a43b6cc3
user <batrakova-olesya@inbox.ru>
2024-09-16 16:13:50 +0300

    parametrize triangle_test.py

--- tests/triangle_test.py
+++ tests/triangle_test.py
@@ -3,36 +3,57 @@ import math
 from src.triangle import Triangle
 
 
-def test_triangle_initialization_valid():
-    triangle = Triangle(13, 14, 15)
-    assert triangle.side_a == 13
-    assert triangle.side_b == 14
-    assert triangle.side_c == 15
+@pytest.mark.parametrize(
+    "side_a, side_b, side_c",
+    [
+        (13, 14, 15),
+    ],
+)
+def test_triangle_initialization_valid(side_a, side_b, side_c):
+    triangle = Triangle(side_a, side_b, side_c)
+    assert triangle.side_a == side_a
+    assert triangle.side_b == side_b
+    assert triangle.side_c == side_c
 
 
-def test_triangle_initialization_invalid():
-    with pytest.raises(ValueError, match="Triangle sides can't be less than or equal to 0"):
-        Triangle(0, 14, 15)
-    with pytest.raises(ValueError, match="Triangle sides can't be less than or equal to 0"):
-        Triangle(13, 0, 15)
-    with pytest.raises(ValueError, match="Triangle sides can't be less than or equal to 0"):
-        Triangle(13, 14, 0)
-    with pytest.raises(ValueError, match="Triangle sides can't be less than or equal to 0"):
-        Triangle(-1, 14, 15)
-    with pytest.raises(ValueError, match="Triangle sides can't be less than or equal to 0"):
-        Triangle(13, -1, 15)
-    with pytest.raises(ValueError, match="Triangle sides can't be less than or equal to 0"):
-        Triangle(13, 14, -1)
+@pytest.mark.parametrize(
+    "side_a, side_b, side_c",
+    [
+        (0, 14, 15),
+        (13, 0, 15),
+        (13, 14, 0),
+        (-1, 14, 15),
+        (13, -1, 15),
+        (13, 14, -1),
+    ],
+)
+def test_triangle_initialization_invalid(side_a, side_b, side_c):
+    with pytest.raises(
+        ValueError, match="Triangle sides can't be less than or equal to 0"
+    ):
+        Triangle(side_a, side_b, side_c)
 
 
-def test_triangle_get_area():
-    triangle = Triangle(13, 14, 15)
-    s = (13 + 14 + 15) / 2
-    expected_area = math.sqrt(s * (s - 13) * (s - 14) * (s - 15))
+@pytest.mark.parametrize(
+    "side_a, side_b, side_c",
+    [
+        (13, 14, 15),
+    ],
+)
+def test_triangle_get_area(side_a, side_b, side_c):
+    triangle = Triangle(side_a, side_b, side_c)
+    s = (side_a + side_b + side_c) / 2
+    expected_area = math.sqrt(s * (s - side_a) * (s - side_b) * (s - side_c))
     assert triangle.get_area() == pytest.approx(expected_area, rel=1e-9)
 
 
-def test_triangle_get_perimeter():
-    triangle = Triangle(13, 14, 15)
-    expected_perimeter = 13 + 14 + 15
+@pytest.mark.parametrize(
+    "side_a, side_b, side_c",
+    [
+        (13, 14, 15),
+    ],
+)
+def test_triangle_get_perimeter(side_a, side_b, side_c):
+    triangle = Triangle(side_a, side_b, side_c)
+    expected_perimeter = side_a + side_b + side_c
     assert triangle.get_perimeter() == expected_perimeter
